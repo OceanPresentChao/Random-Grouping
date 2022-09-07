641f31844d0c09adb34759bc479c7511983de87b
OceanPresent <oceanpresent@163.com>
2022-09-07 15:52:27 +0800

    :sparkles: feat: hash、hmac、ansi algorithm

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,50 +1,69 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { people } from '../utils/people';
-import { linearModRandom } from "../utils/algorithm"
-type RandomAlgorithm = "linearMod"
-const algorithmSelections = ["linearMod"]
-const algorithm = ref<RandomAlgorithm>('linearMod')
+import { computed, ref } from 'vue'
+import { people } from '../utils/people'
+import { ANSIX, hmac, linearMod, uniDirectHash } from '../utils/algorithm'
+type RandomAlgorithm = 'LinearMod' | 'UniDirectHash' | 'Hmac' | 'ANSI'
+const algorithmSelections = ['LinearMod', 'UniDirectHash', 'Hmac', 'ANSI']
+const algorithm = ref<RandomAlgorithm>('LinearMod')
 const origin = ref(people)
 const group = computed(() => {
   const result = []
-  for (let i = 0; i < origin.value.length; i += 3) {
+  for (let i = 0; i < origin.value.length; i += 3)
     result.push([origin.value[i], origin.value[i + 1], origin.value[i + 2]])
-  }
+
   return result
 })
-function randomGroup(algorithm: RandomAlgorithm) {
-  if (algorithm === "linearMod") {
-    origin.value = linearModRandom(origin.value)
-  }
+
+function shuffleArray<T, U>(
+  arr: Array<T>,
+  generator: Generator<U, void, unknown>,
+): Array<T> {
+  const result = arr.concat().map(v => ({ val: v, index: generator.next().value as U }))
+  result.sort((a, b) => {
+    return a.index > b.index ? 1 : -1
+  })
+  return result.map(v => v.val)
 }
 
-
+function randomGroup(algorithm: RandomAlgorithm) {
+  if (algorithm === 'LinearMod')
+    origin.value = shuffleArray(origin.value, linearMod())
+  if (algorithm === 'UniDirectHash')
+    origin.value = shuffleArray(origin.value, uniDirectHash())
+  if (algorithm === 'Hmac')
+    origin.value = shuffleArray(origin.value, hmac())
+  if (algorithm === 'ANSI')
+    origin.value = shuffleArray(origin.value, ANSIX())
+}
 </script>
 
 <template>
   <div class="container">
     <div>
-      <p style="font-size: large;font-weight: 600;">Random Grouping Tool</p>
+      <p style="font-size: large; font-weight: 600">
+        Random Grouping Tool
+      </p>
       <div>
         <span>Algorithm: </span>
-        <select name="algorithm" v-model="algorithm">
-          <option v-for="(val,index) in algorithmSelections" :value="val">{{val}}</option>
+        <select v-model="algorithm" name="algorithm">
+          <option v-for="(val) in algorithmSelections" :key="val" :value="val">
+            {{ val }}
+          </option>
         </select>
       </div>
     </div>
     <div>
-      <div v-for="(v,i) in group" :key="i">
-        <span>Group{{i+1}}:</span>
-        <span v-for="(p) in v">{{p}}</span>
+      <div v-for="(v, i) in group" :key="i">
+        <span>Group{{ i + 1 }}:</span>
+        <span v-for="(p, n) in v" :key="n" style="padding:1em;">{{ p.name }}</span>
       </div>
       <div>
-        <button @click="randomGroup(algorithm)">Click Me</button>
+        <button @click="randomGroup(algorithm)">
+          Click Me
+        </button>
       </div>
     </div>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
--- src/utils/algorithm.ts
+++ src/utils/algorithm.ts
@@ -1,18 +1,124 @@
-export function linearModRandom<T>(arr: Array<T>): Array<T> {
-  function* getRandomNumber() {
-    const seed = Date.now() / 1000
-    const A = 27
-    const C = 39
-    const M = 18
-    let R = (A * seed + C) % M
-    while (1) {
-      yield R = (A * R + C) % M
+import * as cryptojs from 'crypto-js'
+export interface LinearModOption {
+  A?: number
+  C?: number
+  M?: number
+}
+export function* linearMod(options: LinearModOption = {}) {
+  const {
+    A = 27,
+    C = 39,
+    M = 100,
+  } = options
+  const seed = Date.now() / 1000
+  let R = (A * seed + C) % M
+  while (1)
+    yield R = (A * R + C) % M
+}
+
+export interface UnidirectHashOption {
+  hashType?: 'sha1' | 'sha2' | 'sha3' | 'md5'
+}
+
+export function* uniDirectHash(options: UnidirectHashOption = {}) {
+  const {
+    hashType = 'md5',
+  } = options
+  let seed = Date.now() / 1000
+  while (1) {
+    switch (hashType) {
+      case 'md5': yield cryptojs.MD5(seed.toString()).toString()
+        break
+      case 'sha1': yield cryptojs.SHA1(seed.toString()).toString()
+        break
+      case 'sha2': yield cryptojs.SHA256(seed.toString()).toString()
+        break
+      case 'sha3': yield cryptojs.SHA3(seed.toString()).toString()
+        break
+    }
+    seed++
+  }
+}
+
+export interface HmacOption {
+  hashType?: 'sha1' | 'sha2' | 'sha3' | 'md5'
+}
+
+export function* hmac(options: HmacOption = {}) {
+  const {
+    hashType = 'md5',
+  } = options
+  let seed = Date.now() / 1000
+  const key = getOTP()
+  while (1) {
+    switch (hashType) {
+      case 'md5': yield cryptojs.HmacMD5(seed.toString(), key).toString()
+        break
+      case 'sha1': yield cryptojs.HmacSHA1(seed.toString(), key).toString()
+        break
+      case 'sha2': yield cryptojs.HmacSHA256(seed.toString(), key).toString()
+        break
+      case 'sha3': yield cryptojs.HmacSHA3(seed.toString(), key).toString()
+        break
+    }
+    seed++
+  }
+}
+
+export interface ANSIXOption {
+  hashType?: 'sha1' | 'sha2' | 'sha3' | 'md5'
+}
+
+export function* ANSIX(options: ANSIXOption = {}) {
+  const {
+    hashType = 'md5',
+  } = options
+  const key = getOTP()
+  let seed = Date.now().toString()
+  while (1) {
+    const A = getXOR(seed, key)
+    const result = crypto(A, key)
+    yield result
+    const B = getXOR(result, key)
+    seed = crypto(B, key)
+  }
+  function crypto(message: string, key: string) {
+    switch (hashType) {
+      case 'md5': return cryptojs.HmacMD5(message, key).toString()
+      case 'sha1': return cryptojs.HmacSHA1(message, key).toString()
+      case 'sha2': return cryptojs.HmacSHA256(message, key).toString()
+      case 'sha3': return cryptojs.HmacSHA3(message, key).toString()
     }
   }
-  const generator = getRandomNumber()
-  let result = arr.concat().map((v) => ({ val: v, index: generator.next().value as number }))
-  result.sort((a, b) => {
-    return a.index - b.index;
-  })
-  return result.map((v) => v.val)
+}
+
+function getXOR(message: string, key: string) {
+  const arr = []
+  for (let i = 0; i < message.length && i < key.length; i++) {
+    const m = parseInt(message.substring(i, 1), 16)
+    const k = parseInt(key.substring(i, 1), 16)
+    arr.push((m ^ k).toString(16))
+  }
+  return arr.join('')
+}
+
+// 生成一个随机整数，范围是 [min, max]
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// 生成一个随机的十六进制的值，在 0 ～ f 之间
+function getHex() {
+  let n = 0
+  for (let i = 4; i > 0; i--)
+    n = (getRandomInt(0, 1) << (i - 1)) + n
+  return n.toString(16)
+}
+
+// 生成一个32位的十六进制值，用作一次性 Key
+function getOTP() {
+  const arr = []
+  for (let i = 0; i < 32; i++)
+    arr.push(getHex())
+  return arr.join('')
 }
