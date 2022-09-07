ded4c1e3a985947e5db8d7675adca29be63878fb
OceanPresent <oceanpresent@163.com>
2022-09-07 22:10:06 +0800

    :sparkles: feat: modify group size && output all

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import { BaseDirectory, writeTextFile } from '@tauri-apps/api/fs'
 import { people } from '../utils/people'
 import { ANSIX, hmac, linearMod, uniDirectHash } from '../utils/algorithm'
 type RandomAlgorithm = 'LinearMod' | 'UniDirectHash' | 'Hmac' | 'ANSI'
 const algorithmSelections = ['LinearMod', 'UniDirectHash', 'Hmac', 'ANSI']
 const algorithm = ref<RandomAlgorithm>('LinearMod')
 const origin = ref(people)
+const groupSize = ref(6)
+const isHandled = ref(false)
+const cache: Array<typeof origin.value> = []
 const group = computed(() => {
   const result = []
-  for (let i = 0; i < origin.value.length; i += 3)
-    result.push([origin.value[i], origin.value[i + 1], origin.value[i + 2]])
-
+  const groupNum = Math.ceil(origin.value.length / groupSize.value)
+  let tmp = []
+  for (let i = 0; i < origin.value.length; i++) {
+    tmp.push(origin.value[i])
+    if ((i + 1) % groupSize.value === 0 || i === origin.value.length - 1) {
+      result.push(tmp.concat())
+      tmp = []
+    }
+  }
   return result
 })
 
@@ -26,6 +36,8 @@ function shuffleArray<T, U>(
 }
 
 function randomGroup(algorithm: RandomAlgorithm) {
+  if (!isHandled.value)
+    isHandled.value = true
   if (algorithm === 'LinearMod')
     origin.value = shuffleArray(origin.value, linearMod())
   if (algorithm === 'UniDirectHash')
@@ -34,6 +46,13 @@ function randomGroup(algorithm: RandomAlgorithm) {
     origin.value = shuffleArray(origin.value, hmac())
   if (algorithm === 'ANSI')
     origin.value = shuffleArray(origin.value, ANSIX())
+  for (let i = 0; i < origin.value.length; i++)
+    origin.value[i].group = Math.ceil((i + 1) / groupSize.value)
+  cache.push(origin.value.concat())
+}
+
+function outPutCache() {
+  writeTextFile('output.json', JSON.stringify(cache), { dir: BaseDirectory.Desktop })
 }
 </script>
 
@@ -50,6 +69,9 @@ function randomGroup(algorithm: RandomAlgorithm) {
             {{ val }}
           </option>
         </select>
+        <span style="padding: 1em;" />
+        <label>GroupSize: </label>
+        <input v-model="groupSize" type="text">
       </div>
     </div>
     <div>
@@ -59,7 +81,11 @@ function randomGroup(algorithm: RandomAlgorithm) {
       </div>
       <div>
         <button @click="randomGroup(algorithm)">
-          Click Me
+          Random
+        </button>
+        <span style="padding: 1em;" />
+        <button :disabled="!isHandled" @click="outPutCache()">
+          Output
         </button>
       </div>
     </div>
--- src/utils/people.ts
+++ src/utils/people.ts
@@ -1,55 +1,74 @@
 export const people = [
   {
-    name: "潮海波"
+    name: '潮海波',
+    group: 1,
   },
   {
-    name: "李柯凡"
+    name: '李柯凡',
+    group: 1,
   },
   {
-    name: "许嘉豪"
+    name: '许嘉豪',
+    group: 1,
   },
   {
-    name: "鞠昌毅"
+    name: '鞠昌毅',
+    group: 1,
   },
   {
-    name: "史昊原"
+    name: '史昊原',
+    group: 1,
   },
   {
-    name: "李炫辰"
+    name: '李炫辰',
+    group: 1,
   },
   {
-    name: "张庆霄"
+    name: '张庆霄',
+    group: 1,
   },
   {
-    name: "龙健超"
-  },{
-    name: "韩强"
-  }
-  ,{
-    name: "单嘉澳"
-  }
-  ,{
-    name: "刘湘增"
-  }
-  ,{
-    name: "付轩轩"
-  }
-  ,{
-    name: "吴国梁"
+    name: '龙健超',
+    group: 1,
   },
   {
-    name: "王硕"
+    name: '韩强',
+    group: 1,
   },
   {
-    name: "张昱昊"
+    name: '单嘉澳',
+    group: 1,
   },
   {
-    name: "孙兰博"
+    name: '刘湘增',
+    group: 1,
   },
   {
-    name: "谢志伟"
+    name: '付轩轩',
+    group: 1,
   },
   {
-    name: "赵博"
-  }
+    name: '吴国梁',
+    group: 3,
+  },
+  {
+    name: '王硕',
+    group: 3,
+  },
+  {
+    name: '张昱昊',
+    group: 3,
+  },
+  {
+    name: '孙兰博',
+    group: 3,
+  },
+  {
+    name: '谢志伟',
+    group: 3,
+  },
+  {
+    name: '赵博',
+    group: 3,
+  },
 ]
